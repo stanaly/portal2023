4524496cb8cb42aff788cef5815822c3e391d933
stanaly <tlsgusrb70@gmail.com>
2023-04-07 19:26:32 +0900

    6주차 수업

--- userdao2/src/main/java/kr/ac/jejunu/user/HallaConnectionMaker.java
+++ userdao2/src/main/java/kr/ac/jejunu/user/HallaConnectionMaker.java
@@ -7,10 +7,11 @@ import java.sql.SQLException;
 public class HallaConnectionMaker implements ConnectionMaker {
     @Override
     public Connection getConnection() throws ClassNotFoundException, SQLException {
-        // 데이터 어딨어? mysql
-        // mysql 클래스 로딩
+        //데이터 어딨어? mysql
+        //mysql 클래스 로딩
         Class.forName("com.mysql.cj.jdbc.Driver");
-        // Connection 맺고
-        return DriverManager.getConnection("jdbc:mysql://localhost/halla", "jeju", "jejupw");
+        //Connection 맺고
+        return DriverManager.getConnection
+                ("jdbc:mysql://192.168.151.175/halla", "jeju", "jejupw");
     }
 }
