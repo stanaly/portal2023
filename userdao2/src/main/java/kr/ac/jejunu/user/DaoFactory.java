8eb7ace81a0590da0d96571edfd828c4b247543e
stanaly <tlsgusrb70@gmail.com>
2023-05-12 17:48:07 +0900

    11주차

--- userdao2/src/main/java/kr/ac/jejunu/user/DaoFactory.java
+++ userdao2/src/main/java/kr/ac/jejunu/user/DaoFactory.java
@@ -3,6 +3,7 @@ package kr.ac.jejunu.user;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 
 import javax.sql.DataSource;
@@ -19,16 +20,16 @@ public class DaoFactory {
     @Value("${db.url}")
     private String url;
 
-    @Bean
-    public UserDao userDao() throws ClassNotFoundException {
-        UserDao userDao = new UserDao(jdbcContext());
-        return userDao;
-    }
+//    @Bean
+//    public UserDao userDao() throws ClassNotFoundException {
+//        UserDao userDao = new UserDao(jdbcTemplate());
+//        return userDao;
+//    }
 
     @Bean
-    public JdbcContext jdbcContext() throws ClassNotFoundException {
-        JdbcContext jdbcContext = new JdbcContext(dataSource());
-        return jdbcContext;
+    public JdbcTemplate jdbcTemplate() throws ClassNotFoundException {
+        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource());
+        return jdbcTemplate;
     }
 
     @Bean
--- userdao2/src/main/java/kr/ac/jejunu/user/UserDao.java
+++ userdao2/src/main/java/kr/ac/jejunu/user/UserDao.java
@@ -1,36 +1,64 @@
 package kr.ac.jejunu.user;
 
-import javax.sql.DataSource;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.core.ResultSetExtractor;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Component;
+
 import java.sql.*;
 
+@Component
+@RequiredArgsConstructor
 public class UserDao {
-    private final JdbcContext jdbcContext;
-
-    public UserDao(JdbcContext jdbcContext) {
-        this.jdbcContext = jdbcContext;
-    }
-
-    public User findById(Long id) throws ClassNotFoundException, SQLException {
-        StatementStrategy statementStrategy = new FindStatementStrategy(id);
-        return jdbcContext.jdbcContextForFind(statementStrategy);
+    private final JdbcTemplate jdbcTemplate;
+    public User findById(Long id) {
+        String sql = "select id, name, password from userinfo where id = ?";
+        Object[] params = new Object[]{id};
+        return jdbcTemplate.query(sql, rs -> {
+            User user = null;
+            if(rs.next()) {
+                user = new User();
+                user.setId(rs.getLong("id"));
+                user.setName(rs.getString("name"));
+                user.setPassword(rs.getString("password"));
+            }
+            return user;
+        }, params);
     }
 
     public void insert(User user) throws SQLException {
-        StatementStrategy statementStrategy = new InsertStatementStrategy(user);
-        jdbcContext.jdbcContextForInsert(user, statementStrategy);
+        String sql = "insert into userinfo (name, password) values ( ?, ? )";
+        Object[] params = new Object[] {user.getName(), user.getPassword()};
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        jdbcTemplate.update(con -> {
+            PreparedStatement preparedStatement = con.prepareStatement (sql, Statement.RETURN_GENERATED_KEYS);
+            for(int i = 0; i< params.length; i++) {
+                preparedStatement.setObject(i+1, params[i]);
+            }
+            return preparedStatement;
+        }, keyHolder );
+        user.setId(keyHolder.getKey().longValue());
     }
 
     public void update(User user) throws SQLException {
-        StatementStrategy statementStrategy = new UpdateStatementStrategy(user);
-        jdbcContext.jdbcContextForUpdate(statementStrategy);
+        String sql = "update userinfo set name = ?, password = ? where id = ?";
+        Object[] params = new Object[]{user.getName(), user.getPassword(), user.getId()};
+        jdbcTemplate.update(sql, params);
 
     }
 
     public void delete(Long id) throws SQLException {
-        StatementStrategy statementStrategy = new DeleteStatementStrategy(id);
-        jdbcContext.jdbcContextForUpdate(statementStrategy);
+        String sql = "delete from userinfo where id = ?";
+        Object[] params = new Object[]{id};
+        jdbcTemplate.update(sql, params);
 
     }
+
 }
 
 
